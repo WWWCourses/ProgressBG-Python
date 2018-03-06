41a272a40cb1f251c783625ffa4dd386fb3d1b52
WWWCourses <ivawebcourses@gmail.com>
2018-03-06 23:08:02 +0200

    CR year changed; Comments in Python added; Variables section moved

---
+++ projects/course_intro/comments.py
@@ -0,0 +1,6 @@
+# this is a just a comment: no print("ada") will happens
+print("this will be printed, of course") 
+
+### a more semantic example for comment:
+# check if a triangle with sides (3,4,5) is a Pythagorean:
+print(3**2 + 4**2 == 5**2)
