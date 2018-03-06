c6c9a42a6294f900cf903527757c6b843124613a
WWWCourses <ivawebcourses@gmail.com>
2018-03-07 00:11:15 +0200

    Exercises solutions and HWs

--- projects/course_intro/acad_to_astro_hours_with_vars.py
+++ projects/course_intro/acad_to_astro_hours_with_vars.py
@@ -1,22 +1,27 @@
-'''
+"""
 acad_to_astro_hours_with_vars.py
-an example of too many variables in a program
-TODO: upload in slides
-'''
 
+An example of too many variables, with too long names.
+
+
+"""
+
+# given:
 minutes_in_astro_hour = 60
 minutes_in_acad_hour = 40
-acad_course_hourse = 12
+acad_course_hours = 64
+acad_hours_in_a_partida = 4
 
-acad_hours_partida = acad_course_hourse/4
+# program logic: 
+acad_hours_partidas = acad_course_hours/acad_hours_in_a_partida
 
-minutes_brakes_in_partida = acad_hours_partida * 20
+total_minutes_brakes = acad_hours_partidas * 20
 
-minutes_in_a_course = acad_course_hourse * minutes_in_acad_hour + minutes_brakes_in_partida
+minutes_in_a_course = acad_course_hours * minutes_in_acad_hour + total_minutes_brakes
 
-course_in_astro_hourse = minutes_in_a_course / 60
+course_in_astro_hours = minutes_in_a_course / 60
 
-print(course_in_astro_hourse)
+print(course_in_astro_hours)
 
 
 
