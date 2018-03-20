1bd1103588be3e3b9e744e9e2d9054a2efb7825b
WWWCourses <ivawebcourses@gmail.com>
2018-03-20 16:58:55 +0200

    dict task

---
+++ pages/themes/beginners/dictAndSetDataTypes/Tasks_and_HW/task1_students_scores.py
@@ -0,0 +1,11 @@
+students = {
+    "Ivan": 5.00,
+    "Alex": 3.50,
+    "Maria": 5.50,
+    "Georgy": 5.00,
+}
+
+# Print out the names of the students, which scores > 4.00
+for name in students:
+    if( students[name] > 4.00):
+        print(name)
---
+++ pages/themes/beginners/dictAndSetDataTypes/Tasks_and_HW/task2_students_scores.py
@@ -0,0 +1,15 @@
+students = {
+    "Ivan": 5.00,
+    "Alex": 3.50,
+    "Maria": 5.50,
+    "Georgy": 5.00,
+}
+
+# Print out the names of the students, which scores > 4.00
+names = list(students.keys())
+scores = list(students.values())
+
+max_score = max(scores)
+max_score_index = scores.index(max_score)
+
+print("{} - {}".format(names[max_score_index], max_score))
