4f0bed4dc9f3996a0b771d6e02d0f406c79e0d8a
WWWCourses <ivawebcourses@gmail.com>
2018-04-19 14:28:09 +0300

    First Working View

--- projects/course_intro/raw_input.py
+++
@@ -1,9 +0,0 @@
-machine_number = 43
-
-
-user_number = int( raw_input("enter a number: ") )
-
-print( type(user_number) ) 
-
-if user_number == machine_number:
-	print("Bravo!!!")
--- projects/course_intro/say_hello.py
+++
@@ -1,2 +0,0 @@
-name = "iva"
-print("Hello" + name + "!")
